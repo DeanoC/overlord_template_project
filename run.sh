59ab50210b29c5699e28d0d03408f48f5beb1965
deanoc <me@deanoc.com>
2025-04-06 09:40:20 +0000

    Initial commit with template project files

---
+++ run.sh
@@ -0,0 +1,7 @@
+(cd ~/overlord && sbt compile)
+if [ $? -eq 0 ]; then
+  (cd ~/overlord && sbt assembly)
+  if [ $? -eq 0 ]; then
+    java -cp ~/overlord/target/scala-3.3.1/overlord-assembly-1.0.jar Main create test.over --board kv260
+  fi
+fi
