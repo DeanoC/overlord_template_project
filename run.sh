6cf3fe4c888dd59057949cfceac84fa37617f43a
deanoc <me@deanoc.com>
2025-04-06 09:47:50 +0000

    Update run.sh to create template_project.over instead of test.over

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 if [ $? -eq 0 ]; then
   (cd ~/overlord && sbt assembly)
   if [ $? -eq 0 ]; then
-    java -cp ~/overlord/target/scala-3.3.1/overlord-assembly-1.0.jar Main create test.over --board kv260
+    java -cp ~/overlord/target/scala-3.3.1/overlord-assembly-1.0.jar Main create template_project.over --board kv260
   fi
 fi
